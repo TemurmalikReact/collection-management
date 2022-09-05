862615d22cf3a4d4d33e5b176d8e761946f7770d
TemurmalikReact <temurmalikreact@gmail.com>
2022-09-05 11:27:50 +0500

    port

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ app.use('/api/users', require('./routes/users.routes'))
 app.use('/api/collections', require('./routes/collection.routes'))
 app.use('/api/items', require('./routes/item.routes'))
 
-const PORT = process.env.PORT || config.get("port") || 5000;
+const PORT = config.get("port") || 5000;
 
 async function start() { 
   try {
     await mongoose.connect(config.get("mongoUri"));
-    app.listen(PORT, () => console.log(`App has been started on port ${PORT}`));
+    app.listen(process.env.PORT || PORT, () => console.log(`App has been started on port ${PORT}`));
     
   } catch (e) {
     console.log("Server error", e.message);
