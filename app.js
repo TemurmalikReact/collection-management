3f7716de9e04af2444cd73f56a16b317e5275449
TemurmalikReact <temurmalikreact@gmail.com>
2022-09-05 11:13:17 +0500

    heroku

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use('/api/users', require('./routes/users.routes'))
 app.use('/api/collections', require('./routes/collection.routes'))
 app.use('/api/items', require('./routes/item.routes'))
 
-const PORT = config.get("port") || 5000;
+const PORT = process.env.PORT || config.get("port") || 5000;
 
 async function start() { 
   try {
