13e43a6da8306e12c73d702b2d8e259471638896
TemurmalikReact <temurmalikreact@gmail.com>
2022-09-05 12:13:54 +0500

    build

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const config = require("config");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
@@ -13,11 +14,19 @@ app.use('/api/items', require('./routes/item.routes'))
 
 const PORT = config.get("port") || 5000;
 
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, "client", "build")))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
+
 async function start() { 
   try {
     await mongoose.connect(config.get("mongoUri"));
-    app.listen(process.env.PORT || PORT, () => console.log(`App has been started on port ${PORT}`));
-    
+    app.listen(PORT, () => console.log(`App has been started on port ${PORT}`));
+
   } catch (e) {
     console.log("Server error", e.message);
     process.exit(1);
