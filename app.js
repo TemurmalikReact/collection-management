1d8bc7d82c368716315e03e1cdcfc68bbfdf7f2f
TemurmalikReact <temurmalikreact@gmail.com>
2022-09-05 11:23:26 +0500

    PORT fixed

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const PORT = process.env.PORT || config.get("port") || 5000;
 async function start() { 
   try {
     await mongoose.connect(config.get("mongoUri"));
-    app.listen(5000, () => console.log(`App has been started on port ${PORT}`));
+    app.listen(PORT, () => console.log(`App has been started on port ${PORT}`));
+    
   } catch (e) {
     console.log("Server error", e.message);
     process.exit(1);
